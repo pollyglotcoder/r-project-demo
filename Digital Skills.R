717dae00ac6e44b7d6532e8940e3cb1bb06b4d22
XUEKE HOU <wei@example.com>
2025-10-26 15:23:35 +0000

    Initial commit:R-Project-Digital Skills

---
+++ Digital Skills.R
@@ -0,0 +1,20 @@
+################################################################################################################################################################################################
+# 1.load the library
+library(dplyr)
+library(ggplot2)
+library(broom)
+library(skimr)
+library(interactions)
+library(tidyverse)
+library(haven)
+library(scales)
+library(car)
+library(rpart)
+library(rpart.plot)
+library(glmnet)
+library(randomForest)
+library(MASS)
+library(Metrics)
+library(stargazer)
+################################################################################################################################################################################################
+# 2. Import data
